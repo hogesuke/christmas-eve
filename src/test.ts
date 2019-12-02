d8fef2abba601a79f8130d5e8b5e88bd4322df81
hogesuke <miyado@gmail.com>
2019-12-02 23:13:15 +0900

    Adopt Parcel

---
+++ src/test.ts
@@ -0,0 +1,15 @@
+class Hoge {
+    private message: string;
+
+    constructor(message: string) {
+        this.message = message;
+    }
+
+    hello() {
+        return 'Hello!' + this.message;
+    }
+}
+
+const hoge = new Hoge('へいへいへい！');
+console.log(hoge.hello());
+
