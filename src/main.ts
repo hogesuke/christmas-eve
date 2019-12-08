07d2dc17757532bea2fd3bb6687425504579c92d
hogesuke <miyado@gmail.com>
2019-12-08 15:57:42 +0900

    Reduce file size

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,13 @@
-import * as THREE from 'three';
-import { Vector2 } from 'three';
+import {
+  WebGLRenderer,
+  PerspectiveCamera,
+  Scene,
+  PointLight,
+  BoxGeometry,
+  Vector2,
+  FontLoader,
+  Font
+} from 'three';
 import SnowMeshFactory from './snow-mesh-factory';
 import CommitLogMesh from './snow-mesh';
 import CommitLog from './commit-log';
@@ -8,11 +16,11 @@ class Canvas {
   private w: number;
   private h: number;
   private mouse: Vector2;
-  private renderer: THREE.WebGLRenderer;
-  private camera: THREE.PerspectiveCamera;
-  private scene: THREE.Scene;
-  private light: THREE.PointLight;
-  private geo: THREE.BoxGeometry;
+  private renderer: WebGLRenderer;
+  private camera: PerspectiveCamera;
+  private scene: Scene;
+  private light: PointLight;
+  private geo: BoxGeometry;
   private snowMeshes: CommitLogMesh[] = [];
 
   constructor (w: number, h: number) {
@@ -26,7 +34,7 @@ class Canvas {
     this.mouse = new Vector2(0, 0);
 
     // レンダラーを作成
-    this.renderer = new THREE.WebGLRenderer({ alpha: true });
+    this.renderer = new WebGLRenderer({ alpha: true });
     this.renderer.setSize(this.w, this.h); // 描画サイズ
     this.renderer.setClearColor(0x000000, 1);
     this.renderer.setPixelRatio(window.devicePixelRatio);
@@ -45,39 +53,40 @@ class Canvas {
     const dist = (this.h / 2) / Math.tan(fovRad);
 
     // カメラを作成（視野角、画面のアスペクト比、カメラに映る最短距離、カメラに映る再遠距離）
-    this.camera = new THREE.PerspectiveCamera(fov, this.w / this.h, 1, dist * 2);
+    this.camera = new PerspectiveCamera(fov, this.w / this.h, 1, dist * 2);
     this.camera.position.z = dist; // カメラを遠ざける
 
     // シーンを作成
-    this.scene = new THREE.Scene();
+    this.scene = new Scene();
 
     // ライトを作成
-    this.light = new THREE.PointLight(0x00ffff);
+    this.light = new PointLight(0x00ffff);
     this.light.position.set(0, 0, 400); // ライトの位置を設定
 
     // ライトをシーンに追加
     this.scene.add(this.light);
 
     // 立方体のジオメトリを作成（幅、高さ、奥行き）
-    // const geo = new THREE.BoxGeometry(300, 300, 300);
+    // const geo = new BoxGeometry(300, 300, 300);
 
-    const loader = new THREE.FontLoader();
+    const loader = new FontLoader();
 
-    const font = await new Promise<THREE.Font>(resolve => {
-      loader.load('Sawarabi_Mincho_Regular.json', resolve);
+    const font = await new Promise<Font>(resolve => {
+      // loader.load('Sawarabi_Mincho_Regular.json', resolve);
+      loader.load('helvetiker_regular.typeface.json', resolve);
     });
 
     const snowMeshFactory = new SnowMeshFactory(font);
 
-    for (let i = 0; i < 10; i++) {
+    for (let i = 0; i < 100; i++) {
       const commitLog = new CommitLog(`#${i}`);
       const snowMesh = snowMeshFactory.createMesh(commitLog);
 
-      // const x = Math.floor(Math.random() * 2000) - 1000;
-      // const y = Math.floor(Math.random() * 1000);
-      // const z = Math.floor(Math.random() * 2000) - 1000;
+      const x = Math.floor(Math.random() * 2000) - 1000;
+      const y = Math.floor(Math.random() * 1000);
+      const z = Math.floor(Math.random() * 2000) - 1000;
 
-      snowMesh.setInitialPosition(0, 0, 0);
+      snowMesh.setInitialPosition(x, y, z);
 
       this.scene.add(snowMesh.getRawMesh());
       this.snowMeshes.push(snowMesh);
@@ -90,9 +99,9 @@ class Canvas {
   render () {
     requestAnimationFrame(() => { this.render() });
 
-    // const sec = performance.now() / 1000;
+    const sec = performance.now() / 1000;
 
-    // this.snowMeshes.forEach(a => a.setPositionY(a.getInitialPosition().y - sec * 100));
+    this.snowMeshes.forEach(a => a.setPositionY(a.getInitialPosition().y - sec * 100));
 
     this.renderer.render(this.scene, this.camera);
   }
